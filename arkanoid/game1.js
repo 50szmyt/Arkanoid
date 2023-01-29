79f6537c6aa43a6661b76b0d9d0e46996aed9b52
50szmyt <104311842+50szmyt@users.noreply.github.com>
2023-01-29 18:24:54 +0100

    Add files via upload

---
+++ arkanoid/game1.js
@@ -0,0 +1,180 @@
+
+const gameElement = document.getElementById('arkanoid-1');
+const refElements = gameElement.querySelectorAll('[ref]');
+
+
+const store=
+{
+    lifes:2,
+    score:0
+}
+
+const acc={};
+for( let element of refElements){
+    let key=`${element.getAttribute('ref')}Element`;
+    acc[key]=element;
+}
+
+
+const {
+    arenaElement,
+    ballElement,
+    bricksElement,
+    lifesElement,
+    paddleElement,
+    scoreElement,
+    buttonElement,
+}= acc;
+
+Object.assign(paddleElement.style,{
+    left: `${(arenaElement.offsetWidth-paddleElement.offsetWidth)/2}px`
+});
+
+Object.assign(ballElement.style,{
+    left: `${(arenaElement.offsetWidth-ballElement.offsetWidth)/2}px`,
+    top: `${(arenaElement.offsetHeight-(2*ballElement.offsetHeight)-paddleElement.offsetHeight)}px`
+    
+});
+
+
+
+let
+{
+    width: arenaWidth,
+    height: arenaheight,
+    left: arenaLeft,
+    right: arenaRight,
+    top: arenaTop,
+} = arenaElement.getBoundingClientRect();
+
+
+
+let deltaY=1;
+let deltaX=1;
+
+let tout = setInterval( function()
+{
+    scoreElement.innerHTML=store.score;
+    lifesElement.innerHTML=store.lifes;
+
+    let left =ballElement.offsetLeft+deltaX;
+    let top =ballElement.offsetTop-deltaY;
+
+    if(arenaWidth<ballElement.offsetLeft+24)
+    {
+        deltaX=-1;
+    }
+    if(top<0)
+    {
+        deltaY=-1;
+        
+    }
+    if(arenaheight<top+24)
+    {
+        deltaY=1;
+        store.lifes-=1;
+        lifesElement.innerHTML;
+        if(store.lifes<0)
+        {
+            alert("przegrałes");
+            clearInterval(tout);
+        };
+        
+    }
+    if(left<0)
+    {
+        deltaX=1;
+    }
+
+    ballElement.style.left=`${left}px`;
+    ballElement.style.top=`${top}px`;
+
+    //dodane z kulki
+    let ballleft =ballElement.offsetLeft;
+    let balltop =ballElement.offsetTop;
+    
+    const element =document.elementFromPoint( arenaLeft +2+ ballleft,arenaTop +balltop);
+    const element4 =document.elementFromPoint( arenaLeft + ballleft+22,arenaTop +balltop+24);
+    const element1 =document.elementFromPoint( arenaLeft + ballleft+24,arenaTop +balltop+2);
+    const element2 =document.elementFromPoint( arenaLeft + ballleft,arenaTop +balltop+22);
+    //bricks
+
+    if(element.classList.contains('brick')) 
+    {
+        element.classList.add('hide');
+        deltaY*=-1; 
+        store.score+=1;
+        scoreElement.innerHTML;
+    }
+
+    if(element4.classList.contains('brick')) 
+    {
+        element4.classList.add('hide');
+        deltaY*=-1; 
+        store.score+=1;
+        scoreElement.innerHTML;
+    }
+    
+    if(element1.classList.contains('brick')) 
+    {
+        element1.classList.add('hide');
+        deltaY*=-1; 
+        store.score+=1;
+        scoreElement.innerHTML;
+    }
+    if(element2.classList.contains('brick')) 
+    {
+        element2.classList.add('hide');
+        deltaY*=-1; 
+        store.score+=1;
+        scoreElement.innerHTML;
+    }
+
+    //paddle
+    if(element1.classList.contains('paddle')) 
+    {
+        
+        deltaY*=-1; 
+    }
+    if(element2.classList.contains('paddle')) 
+    {
+        
+        deltaY*=-1; 
+    }
+
+
+},10
+);
+
+
+const onMove= function (e)
+{
+    let min=0;
+    let max=arenaWidth-paddleElement.offsetWidth;
+    let point = Math.min(max, Math.max(min, e.pageX-arenaLeft));
+
+    paddleElement.style.left=(point)+ 'px';
+}
+
+paddleElement.addEventListener('mousedown',function(e)
+{
+    document.addEventListener('mousemove', onMove);
+})
+
+
+document.addEventListener('mouseup',function()
+{
+    document.removeEventListener('mousemove', onMove);
+})
+
+
+const fragment = document.createDocumentFragment();
+
+
+const brick=`<div class="brick"></div>`;
+const template = document.createElement('template');
+template.innerHTML= brick.repeat(30);
+bricksElement.appendChild(template.content);
+
+
+
